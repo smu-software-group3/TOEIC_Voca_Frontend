3a1e8b452a1444443b98c781585d633b100ac91c
yoots50 <202210947@sangmyung.kr>
2026-04-12 15:47:36 +0900

    회원가입 페이지 라우팅 설정 및 종속성 수정

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => navigate('/login')}>Login</button>
+      <button onClick={() => navigate('/register')}>Register</button>
     </div>
   );
 }
